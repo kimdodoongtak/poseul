03ca8ce8ccd734dd08146eb9d9828c38225067e4
kimdodoongtak <127913780+kimdodoongtak@users.noreply.github.com>
2025-11-05 16:24:30 +0900

    Refactor Tab1 component structure and imports

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,11 +1,25 @@
-<IonContent>
-  <IonHeader collapse="condense">
-    <IonToolbar>
-      <IonTitle size="large">포술 🧃</IonTitle>
-    </IonToolbar>
-  </IonHeader>
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab1.css';
 
-  <IonText color="primary">
-    <h2>안녕, 뚱딱앱 세상에 오신 걸 환영합니다 🎉</h2>
-  </IonText>
-</IonContent>
+const Tab1: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Tab 1</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Tab 1</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer name="Tab 1 page" />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab1;
